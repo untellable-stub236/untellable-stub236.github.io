c8dc728f48312bc8b507532062a24120aeec98af
CeloHT <celoht3@gmail.com>
2026-07-21 16:02:25 -0400

    Create page.tsx

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,10 @@
+import ContactForm from "@/components/forms/ContactForm";
+
+export default function ContactPage() {
+  return (
+    <section className="container mx-auto py-16">
+      <h1 className="text-4xl font-bold mb-6">Contact Us</h1>
+      <ContactForm />
+    </section>
+  );
+}
