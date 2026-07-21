dfae8c4afe05aebf69b64f02d031dbfd72569289
CeloHT <celoht3@gmail.com>
2026-07-21 17:48:19 -0400

    Create site.ts

---
+++ src/config/site.ts
@@ -0,0 +1,18 @@
+export const siteConfig = {
+  name: "CeloHT",
+  description:
+    "CeloHT is a global Web3 initiative focused on blockchain education, financial inclusion, agent networks, and environmental reforestation.",
+  url: "https://www.celoht.com",
+  ogImage: "/og-image.png",
+  twitterImage: "/twitter-image.png",
+  keywords: [
+    "CeloHT",
+    "Web3",
+    "Blockchain Education",
+    "Financial Inclusion",
+    "cUSD",
+    "Valora",
+    "Reforestation",
+    "Agent Network"
+  ]
+};
