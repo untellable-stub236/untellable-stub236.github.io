e55ca1ed5f7e44bcafeb8e85cf511a87278cb241
CeloHT <celoht3@gmail.com>
2026-07-21 18:29:15 -0400

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: "class",
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#00a884",
+        secondary: "#f4f4f4"
+      }
+    }
+  },
+  plugins: []
+};
