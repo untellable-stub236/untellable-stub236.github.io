f016f8401931a2f4313cce8c9bf0d3f7e8528228
CeloHT <celoht3@gmail.com>
2026-07-21 17:26:56 -0400

    Create Footer.tsx

---
+++ src/components/navigation/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="py-8 border-t text-center text-gray-600">
+      © {new Date().getFullYear()} CeloHT — All Rights Reserved
+    </footer>
+  );
+}
