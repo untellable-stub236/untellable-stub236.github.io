695c80ecd733e1a66c7a03889db8723e300d268f
CeloHT <celoht3@gmail.com>
2026-07-21 18:15:59 -0400

    Create constants.ts

---
+++ src/utils/constants.ts
@@ -0,0 +1,11 @@
+export const APP_NAME = "CeloHT";
+
+export const DEFAULT_LANGUAGE = "en";
+
+export const SUPPORTED_LANGUAGES = ["en", "ht", "es", "fr"];
+
+export const API_ENDPOINTS = {
+  contact: "/contact",
+  analytics: "/analytics",
+  newsletter: "/newsletter"
+};
