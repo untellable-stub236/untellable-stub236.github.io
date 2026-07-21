8d16330035fe994ad8865d585ffc0191df7b61c9
CeloHT <celoht3@gmail.com>
2026-07-21 17:12:01 -0400

    Create Layout.tsx

---
+++ src/components/layout/Layout.tsx
@@ -0,0 +1,12 @@
+import Header from "@/components/navigation/Header";
+import Footer from "@/components/navigation/Footer";
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <Header />
+      <main className="flex-1">{children}</main>
+      <Footer />
+    </div>
+  );
+}
