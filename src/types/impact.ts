7c83cfe1799af4cb6d34cd4d6456f7569bcb71f5
CeloHT <celoht3@gmail.com>
2026-07-21 18:12:18 -0400

    Create impact.ts

---
+++ src/types/impact.ts
@@ -0,0 +1,17 @@
+export interface ImpactMetrics {
+  education: {
+    peopleEducated: number;
+    workshopsHeld: number;
+    digitalCourses: number;
+  };
+  agents: {
+    activeAgents: number;
+    communitiesServed: number;
+    merchantOnboardings: number;
+  };
+  reforestation: {
+    treesPlanted: number;
+    climateEvents: number;
+    greenCommunities: number;
+  };
+}
